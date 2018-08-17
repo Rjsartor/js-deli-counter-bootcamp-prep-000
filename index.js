7453bcad7de82634d331db51b8bb945736a9d869
randal.sartor@gmail.com <randal.sartor@gmail.com>
2018-08-17 16:41:59 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+function takeANumber(katzDeliLine, newPerson) {
+  katzDeliLine.push(newPerson)
+  return `Welcome, ${newPerson}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    return `Currently serving ${katzDeliLine.splice(0,1)}.`
+  } else {
+    return "There is nobody waiting to be served!"
+  }
+}
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty."
+  } else {
+    katzDeliLine.forEach((katzDeliLine))
+    return `The line is currently ${katzDeliLine[i] + 1} ${katzDeliLine[i]}.`
+  }
+}
