0979f4b49844ab66801cf8914183f114b94dcf53
randal.sartor@gmail.com <randal.sartor@gmail.com>
2018-08-17 17:51:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ function nowServing(katzDeliLine) {
 
 function currentLine(katzDeliLine) {
   line = []
-  if (katzDeliLine.length === 0) {
-    return "The line is currently empty."
-  } else {
-    
-    return `The line is currently ${katzDeliLine[i] + 1} ${katzDeliLine[i]}.`
+  function currentLine(katzDeli) {
+
+  for (let i = 0; i < katzDeli.length; i++) {
+    line.push(` `+[i+1]+`. `  + katzDeli[i])
   }
+  return(`The line is currently:` + line);
 }
