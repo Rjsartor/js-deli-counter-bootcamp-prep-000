cded4bcfa193098161c7c9df2f2028341d5c9a0d
randal.sartor@gmail.com <randal.sartor@gmail.com>
2018-08-17 17:53:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ function nowServing(katzDeliLine) {
   }
 }
 
-function currentLine(katzDeliLine) {
+function currentLine(katzDeli) {
   let line = []
- 
+ 	  let i = 0;
+  while (i < katzDeli.length) {
+    line.push(` `+[i+1]+`. `  + katzDeli[i])
+    i++;
+  }
+  if (katzDeli.length === 0) {
+    return "The line is currently empty.";
+  } else
+  return(`The line is currently:` + line);
+}
